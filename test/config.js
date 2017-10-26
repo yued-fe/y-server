0f94ada9d830bdab7e22afdd2df1d7a00325c52b
Zhang Chengrong <zhangchengrong@yuewen.com>
2017-10-26 15:07:08 +0800

    优化插件加载方式: 支持

--- test/config.js
+++ test/config.js
@@ -1,13 +1,14 @@
-'use strict';
-
 module.exports = {
   plugins: [
+    {
+      $name: 'static',
+      staticPaths: __dirname,
+    },
     function (app) {
       console.log('plugin installed'); // eslint-disable-line no-console
 
-      app.use((req, res, next) => {
-        console.log('plugin valid'); // eslint-disable-line no-console
-        res.end();
+      app.get('/', (req, res, next) => {
+        res.send('plugin valid');
       });
     },
   ],
--- test/index.js
+++ test/index.js
@@ -1,3 +1,1 @@
-'use strict';
-
 require('../lib/y-server.js').run(require('path').join(__dirname, 'config.js'));
